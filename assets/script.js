f439ee23a5032047a1c6fa6dffe0c9ba10ad786c
Blitzbomber87 <elias.efly000@gmail.com>
2024-05-07 13:21:39 -0700

    Added code to createTaskCard function

--- assets/script.js
+++ assets/script.js
@@ -4,35 +4,74 @@ let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-
+// Inside function handletaskFormSubmit(event)
+// Lines 148 - 154
 }
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
+// function savetasksToStorage(tasks)
+// Lines 37 - 71
+    const taskCard = $('<div>')
+      .addClass('card task-card draggable my-3')
+      .attr('data-task-id', task.id);
+    const cardHeader = $('<div>').addClass('card-header h4').text(task.name);
+    const cardBody = $('<div>').addClass('card-body');
+    const cardDescription = $('<p>').addClass('card-text').text(task.type);
+    const cardDueDate = $('<p>').addClass('card-text').text(task.dueDate);
+    const cardDeleteBtn = $('<button>')
+      .addClass('btn btn-danger delete')
+      .text('Delete')
+      .attr('data-task-id', task.id);
+    cardDeleteBtn.on('click', handleDeletetask);
 
+    // ? Sets the card background color based on due date. Only apply the styles if the dueDate exists and the status is not done.
+  if (task.dueDate && task.status !== 'done') {
+    const now = dayjs();
+    const taskDueDate = dayjs(task.dueDate, 'DD/MM/YYYY');
+
+    // ? If the task is due today, make the card yellow. If it is overdue, make it red.
+    if (now.isSame(taskDueDate, 'day')) {
+      taskCard.addClass('bg-warning text-white');
+    } else if (now.isAfter(taskDueDate)) {
+      taskCard.addClass('bg-danger text-white');
+      cardDeleteBtn.addClass('border-light');
+    }
+  }
+
+  // ? Gather all the elements created above and append them to the correct elements.
+  cardBody.append(cardDescription, cardDueDate, cardDeleteBtn);
+  taskCard.append(cardHeader, cardBody);
+
+  // ? Return the card so it can be appended to the correct lane.
+  return taskCard;
 }
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
-
+// function printtaskData()
+// Line 73 - 113
 }
 
 // Todo: create a function to handle adding a new task
 function handleAddTask(event){
-
+// function handletaskFormSubmit(event)
+// Lines 140 - 170
 }
 
 // Todo: create a function to handle deleting a task
 function handleDeleteTask(event){
-
+// function handleDeletetask()
+// Lines 116 - 137
 }
 
 // Todo: create a function to handle dropping a task into a new status lane
 function handleDrop(event, ui) {
-
+// function handleDrop(event, ui)
+// Lines 173 - 192
 }
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
-
+// Lines 206 - 220
 });
